53b46004d8b17de3e1f741c062ea44d87c4ed154
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2022-06-24 21:56:01 +0200

    added goodbye info

--- src/dogeaction/app.py
+++ src/dogeaction/app.py
@@ -8,6 +8,7 @@ from actions_toolkit import core, github
 
 from dogeaction.adapters import from_github_context
 from dogeaction.api import DogeApi
+from dogeaction.ascii import goodbye_message
 from dogeaction.models import dogeops as dm
 
 WORKSPACE = Path(os.getenv("GITHUB_WORKSPACE", "/github/workspace"))
@@ -27,8 +28,6 @@ def upload_manifest(manifest: str, ctx: dm.Context) -> Optional[dm.Deployment]:
     api = DogeApi(os.environ["DOGEOPS_API_KEY"])
 
     project = api.project(url=ctx.repo.url)
-    core.info(f"{project}")
-
     with open(manifest) as man:
         spec = yaml.safe_load(man)
         deployment = api.deploy(project_id=project.id, context=ctx, manifest=spec)
@@ -53,3 +52,5 @@ def main():
     deployment = upload_manifest(doge_file, ctx)
     if not deployment:
         core.set_failed(f"{doge_file} does not exist")
+
+    core.info(goodbye_message(deployment))
